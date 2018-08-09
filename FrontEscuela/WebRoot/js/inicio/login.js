541ebc4dd5202d1a0c163b4ac917a4708808fa33
Eduardo <Eduardo@DESKTOP-4UBMCG3>
2018-08-08 23:09:31 -0500

    x

--- FrontEscuela/WebRoot/js/inicio/login.js
+++ FrontEscuela/WebRoot/js/inicio/login.js
@@ -1,8 +1,8 @@
 function valLogin(){
-	alert("entraa");
+	
 	$.get("http://localhost:8080/BackEnd_CoreEsc/resources/user/valuser/"+$("#inputuser").val()+"/"+$("#inputPassword").val(),
 			  function(data){
-		alert("si");
+	
 			    if(data.success.user=='true'){
 			    	$("#principal").load(data.success.url);
 			    }else{
